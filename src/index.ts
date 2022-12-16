a836d490669dcf14a6f13b7ba1ba937bac0223ff
Nick Nance <nance.nick@gmail.com>
2022-12-16 08:45:15 -0500

    initial data loading class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("hello world")
+import { MnistDataset } from "./data";
+
+const data = new MnistDataset();
+data.loadData().then(() => {
+  console.log(data.getTrainData());
+  console.log(data.getTestData());
+});
